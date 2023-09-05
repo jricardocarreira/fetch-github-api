3d5e68da339c2fddd2c5b13e041b3dd040a98527
José Ricardo Carreira <jricardocarreira@gmail.com>
2023-09-05 09:34:55 -0300

    import alterado

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
-import { getUser } from '/src/scripts/services/user.js'
-import { getRepositories } from '/src/scripts/services/repositories.js'
-import { getEvents } from '/src/scripts/services/events.js'
+import { getUser } from './src/scripts/services/user.js'
+import { getRepositories } from './src/scripts/services/repositories.js'
+import { getEvents } from './src/scripts/services/events.js'
 
-import { user } from '/src/scripts/objects/user.js'
-import { screen } from '/src/scripts/objects/screen.js'
+import { user } from './src/scripts/objects/user.js'
+import { screen } from './src/scripts/objects/screen.js'
 
 
 document.getElementById('btn-search').addEventListener("click", () => {
